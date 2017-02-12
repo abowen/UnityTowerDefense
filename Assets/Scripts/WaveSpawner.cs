ac52ab2feae99e6330ea447f9521469034a1480d
Andrew Bowen <andrew.james.bowen@gmail.com>
2017-02-12 21:34:59 +1100

    Finished third episode - enemy spawner

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform EnemyPrefab;
+    public Transform SpawnPoint;
+    public float TimeBetweenWaves = 5.5f;
+    public Text WaveCountdownText;
+
+    private float countdown = 2f;
+    private int waveNumber = 0;
+
+    private void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWaves());
+            countdown = TimeBetweenWaves;
+        }
+
+        WaveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        waveNumber++;
+
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.2f);
+        }
+        Debug.Log("Wave incoming: " + waveNumber);
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(EnemyPrefab, SpawnPoint.position, SpawnPoint.rotation);
+    }
+}
