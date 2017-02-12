a97c4c27b7d0602129da139d928d7e8b53b7990c
Andrew Bowen <andrew.james.bowen@gmail.com>
2017-02-12 20:53:29 +1100

    Finished second episode - Waypoints

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float Speed = 40f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.Points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * Speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (waypointIndex < Waypoints.Points.Length - 1)
+        {
+            waypointIndex++;
+            target = Waypoints.Points[waypointIndex];
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
