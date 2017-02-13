ec884e351824591203a5184f3fff3fc538304dcf
Andrew Bowen <andrew.james.bowen@gmail.com>
2017-02-13 21:56:08 +1100

    Finished fourth episode - turret rotation

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    public Transform Target;
+    public float Range = 15f;
+    public string EnemyTag = "Enemy";
+    public Transform PartToRotate;
+    public float TurnSpeed = 10f;
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(EnemyTag);
+
+        var shortestDistance = Range;
+        Target = null;
+        foreach (var enemy in enemies)
+        {
+            var distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                Target = enemy.transform;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Target == null)
+        {
+            return;
+        }
+
+        Vector3 direction = Target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = Quaternion.Lerp(PartToRotate.rotation, lookRotation, Time.deltaTime * TurnSpeed).eulerAngles;
+        PartToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, Range);
+    }
+
+}
