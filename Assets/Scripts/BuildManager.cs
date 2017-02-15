80aa03e9856baead7d229f188925e861e675eb08
Andrew Bowen <andrew.james.bowen@gmail.com>
2017-02-15 21:15:16 +1100

    Finished sixth episode - turret placement

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager Instance;
+
+    public GameObject StandardTurretPrefab;
+
+    private GameObject turretToBuild;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        turretToBuild = StandardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color HoverColor;
+    private Vector3 Offset = new Vector3(0, 0.5f, 0);
+
+    private Material material;
+    private Color originalColor;
+    private GameObject turret;
+
+    private void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Already there");
+            return;
+        }
+
+        var turretToBuild = BuildManager.Instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + Offset, transform.rotation);
+    }
+
+    private void OnMouseEnter()
+    {
+        originalColor = material.color;
+        material.color = HoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        material.color = originalColor;
+    }
+}
